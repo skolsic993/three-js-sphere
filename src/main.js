45b3b92f9acf1f29e1e795d90daed23ae0c5a5d8
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2026-08-26 19:44:47 +0200

    Add basic elements inside canvas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+import * as THREE from "three";
+
+//Scene
+const scene = new THREE.Scene();
+
+//Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000,
+);
+
+//Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
