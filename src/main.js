34bf9bf0f2f09d2ad5a662db1c2a18c814a7900f
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2026-08-26 19:57:36 +0200

    Add basic cube which is animated

--- src/main.js
+++ src/main.js
@@ -16,3 +16,18 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+//Elements
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 5;
+
+function animate(time) {
+  cube.rotation.x = time / 2000;
+  cube.rotation.y = time / 3000;
+
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
